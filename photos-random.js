4f80b0e891f7729cffbf6f86bc29f633de8cd65e
Alberto Abades Hoyas <alberto.abades@gmail.com>
2015-02-02 14:41:26 +0100

    Primera prueba de imagen en html

---
+++ photos-random.js
@@ -0,0 +1,4 @@
+var arrayFotos = ["http://upload.wikimedia.org/wikipedia/commons/c/c0/Bridgetown1.jpg", "http://commons.wikimedia.org/wiki/Category:Aerial_photographs_of_the_Bahamas#mediaviewer/File:CISTERN-CAY-NEW-VIEW.jpg",
+"http://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Playa_de_Gran_Caim%C3%A1n-Islas_Caim%C3%A1n01.JPG/640px-Playa_de_Gran_Caim%C3%A1n-Islas_Caim%C3%A1n01.JPG"];
+
+
